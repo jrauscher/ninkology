8e555f9846d3b7923eaceaa24d8b64048e7fceda
haininh14127 <nhnguyen@unomaha.edu>
2014-03-21 15:20:57 -0500

    Added ChangeLog and README

---
+++ ChangeLog.md
@@ -0,0 +1,15 @@
+
+Ninkology 1.0 (03-21-2014)
+========
+Bug Fixes: none
+
+Features: 
+- support uploading file or package to web interface for scanning (http:) and send scanned license information as JSON to user’s email
+- support license scanning via command line
+
+Performance improvements: none
+
+Documentation: 
+- Create version number, system overview, copyright information, license information, installation information, code contribution management plan, stakeholder communities, communication, link to examples/mockups, technical specification, and data flow. 
+
+====================
--- src/scanarchive.pl
+++ src/scanarchive.pl
@@ -17,7 +17,6 @@ use Cwd;
 #
 # Options:
 # -k    Do not delete the extracted directory when the program finishes
-# -w	Called from the web interface. Changes output to html
 
 
 
@@ -37,7 +36,6 @@ my $fossology_nomos = "/usr/share/fossology/nomos/agent/nomos";
 
 
 my $argv_keep = 0;
-my $argv_web = 0;
 
 my $archiveExtractor;
 my $tempPathFull;
@@ -54,51 +52,32 @@ my ($x, $z);
 
 if (!$ARGV[0])
 {
-    print "\nUsage: $0 <options> <archive file path>\n\n";
+    print "\nUsage: $0 <archive file path>\n\n";
     exit;
 }
 
 foreach(@ARGV)
 {
-    # Keep the /tmp/ninkology/archive directory after the scan
-    # This causes bugs with the web interface
     if ($_ eq '-k')
     {
         $argv_keep = 1;
     }
-
-    # Web interface call (probably won't be kept around)
-    if ($_ eq '-w')
-    {
-        $argv_web = 1;
-    } 
 }
 
 
 # Extract the archive to $tempPathLocation
-print "\nExtracting from $ARGV[-1]...\n";
-$archiveExtractor = Archive::Extract->new(archive=>$ARGV[-1])
-    or die "\nError: '$ARGV[-1]' is not recognized as an archive file type\n\n";
+print "\nExtracting from $ARGV[0]...\n";
+$archiveExtractor = Archive::Extract->new(archive=>$ARGV[0])
+    or die "\nError: '$ARGV[0]' is not recognized as an archive file type\n\n";
 $archiveExtractor->extract(to => $tempPathLocation);
 $tempPathFull = $archiveExtractor->extract_path;
 print "Extracted archive to $tempPathFull.\n\n";
 
 
 #Open "NINKA_OUTPUT" and "FOSS_OUTPUT" files for output
-if ($argv_web)
-{
-    $ARGV[-1] =~ /.*\/(.*)/;
-    open($output_ninka, '>', "/var/www/processed/$1/NINKA_OUTPUT.html");
-    open($output_foss, '>', "/var/www/processed/$1/FOSS_OUTPUT.html");
+open($output_ninka, '>', "NINKA_OUTPUT.txt");
+open($output_foss, '>', "FOSS_OUTPUT.txt");
 
-    chmod 0755, "/var/www/processed/$1/NINKA_OUTPUT.html";
-    chmod 0755, "/var/www/processed/$1/FOSS_OUTPUT.html";
-}
-else
-{
-    open($output_ninka, '>', "$ENV{HOME}/Ninka_out.txt") or die $!;
-    open($output_foss, '>', "$ENV{HOME}/FOSSology_out.txt") or die $!;
-}
 
 #Get the list of all files in the directory and subdirectories
 chdir $tempPathFull;
@@ -108,17 +87,10 @@ chdir $tempPathFull;
 # Process each file
 $x = 1;
 $z = scalar(@findStar);
-
-if ($argv_web)
-{
-    print $output_ninka "<pre>";
-    print $output_foss "<pre>";
-}
-
 print "Running FOSSology/Ninka scan...\n";
 foreach (@findStar)
 {
-    printf "\rProcessing file %s/%s...", $x, $z;
+    print "\rProcessing file $x/$z...";
     
     # Scan the file with Ninka
     print $output_ninka `perl $ninka $_`;
@@ -127,12 +99,6 @@ foreach (@findStar)
     print $output_foss `$fossology_nomos $_`;
     
     $x++;
-}
-
-if ($argv_web)
-{
-    print $output_ninka "</pre>";
-    print $output_foss "</pre>";
 }
 
 printf "\n\nScan complete (%s seconds).\n", time - $startTime;
